2b2b13425d27427788269d435626fe7568b95894
Kacper <nusbajzel@gmail.com>
2021-03-16 21:22:02 +0100

    added sneaking and chopping

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,8 +7,11 @@ public class NewBehaviourScript : MonoBehaviour
     public SpriteRenderer sprite;
     public Animator animator;
     public float movement_speed = 1;
+    public float sneaking_speed = .25f;
 
     Vector2 movement_direction = new Vector2(0, 0);
+    private bool sneaking = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +23,15 @@ public class NewBehaviourScript : MonoBehaviour
     {
         movement_direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         movement_direction.Normalize();
-
-        transform.position += new Vector3(movement_direction.x, movement_direction.y, 0) * Time.deltaTime * movement_speed;
+        float speed;
+        if (!sneaking)
+        {
+            speed = movement_speed;
+        } else
+        {
+            speed = sneaking_speed;
+        }
+        transform.position += new Vector3(movement_direction.x, movement_direction.y, 0) * Time.deltaTime * speed;
         
         if (movement_direction.x < 0)
         {
@@ -34,11 +44,36 @@ public class NewBehaviourScript : MonoBehaviour
 
         if (movement_direction.magnitude > 0)
         {
-            animator.SetBool("running", true);
+            if (!sneaking)
+            {
+                animator.SetBool("running", true);
+                animator.SetBool("sneaking", false);
+            } else
+            {
+                animator.SetBool("running", false);
+                animator.SetBool("sneaking", true);
+            }
         }
         else
         {
-            animator.SetBool("running", false);
+            if (sneaking)
+            {
+                animator.SetBool("sneaking", false);
+            } else
+            {
+                animator.SetBool("running", false);
+            }
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.SetTrigger("attack");
+            Debug.Log("attack trigger");
+        }
+
+        if (Input.GetButtonDown("Sneak"))
+        {
+            sneaking = !sneaking;
         }
     }
 }
